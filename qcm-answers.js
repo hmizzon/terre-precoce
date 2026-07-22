d747bf9baf46f8bd28b0da327bd6dfe0108c462d
hmizzon <hugaumizzon@gmail.com>
2026-07-22 13:51:28 +0200

    Add files via upload

---
+++ qcm-answers.js
@@ -0,0 +1,38 @@
+// Réponses aux questions des QCM
+
+const qcmData = {
+
+    6: [
+
+        {
+            text: "Quelle est la planète la plus proche du Soleil ?",
+            answers: [
+                { text: "Vénus", correct: false },
+                { text: "Mercure", correct: true },
+                { text: "Terre", correct: false },
+                { text: "Mars", correct: false }
+            ]
+        },
+        {
+            text: "Quel est le plus grand océan de la Terre ?",
+            answers: [
+                { text: "Océan Atlantique", correct: false },
+                { text: "Océan Indien", correct: false },
+                { text: "Océan Pacifique", correct: true },
+                { text: "Océan Arctique", correct: false }
+            ]
+        },
+        {
+            text: "Quel gaz est le plus abondant dans l'atmosphère terrestre ?",
+            answers: [
+                { text: "Le dioxygène", correct: false },
+                { text: "Le diazote", correct: true },
+                { text: "Dioxyde de carbone", correct: false },
+                { text: "L'ozone", correct: false }
+            ]
+        }
+
+    ],
+
+
+};
